4c272b9e07e571a0cb75c9f99f2d0f9898213794
TusharPatil010010 <tusharp010010@gmail.com>
2020-10-25 16:00:11 +0530

    TC1.1 : Number of records match

--- src/test/java/statecensusanalyser/StateCensusAnalyserTest.java
+++ src/test/java/statecensusanalyser/StateCensusAnalyserTest.java
@@ -10,5 +10,17 @@ import statecensusanalyser.StateCensusAnalyser;
 import java.io.IOException;
 
 public class StateCensusAnalyserTest {
+	public static final String STATECENSUS_CSVFILE = "C:\\Users\\LENOVO\\eclipse-workspace\\statecensusanalyser\\src\\main\\resources\\IndiaStateCensusData.csv";
 
+	@Test
+	public void GivenTheStateCodesCsvFile_IfHasCorrectNumberOfRecords_ShouldReturnTrue() throws IOException {
+		StateCensusAnalyser stateCensusAnalyser = new StateCensusAnalyser();
+		try {
+			int count = stateCensusAnalyser.openCsvBuilder(STATECENSUS_CSVFILE, StateCensus.class);
+			System.out.println(count);
+			Assert.assertEquals(29, count);
+		} catch (CensusAnalyserException e) {
+			e.printStackTrace();
+		}
+	}
 }
