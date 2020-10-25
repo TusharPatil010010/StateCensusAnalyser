2c28c4cc3d425b7404d01da5774958acf7f315a7
TusharPatil010010 <tusharp010010@gmail.com>
2020-10-25 16:10:03 +0530

    TC1.3 : CSV file if incorrect type returns a custom exception

--- src/test/java/statecensusanalyser/StateCensusAnalyserTest.java
+++ src/test/java/statecensusanalyser/StateCensusAnalyserTest.java
@@ -11,6 +11,7 @@ import java.io.IOException;
 
 public class StateCensusAnalyserTest {
 	public static final String STATECENSUS_CSVFILE = "C:\\Users\\LENOVO\\eclipse-workspace\\statecensusanalyser\\src\\main\\resources\\IndiaStateCensusData.csv";
+	public static final String STATECODES_CSVFILE = null;
 	public static final String WRONG_FILE = "/novalue.txt";
 
 	@Test
@@ -34,4 +35,14 @@ public class StateCensusAnalyserTest {
 			Assert.assertEquals(CensusAnalyserException.CensusExceptionType.NO_SUCH_FILE, e.type);
 		}
 	}
+	
+    @Test
+    public void GivenTheStateCensusCsvFile_WhenCorrect_ButFileExtensionIncorrect_ShouldThrowCensusAnalyserException() throws IOException {
+        try {
+            int count = StateCensusAnalyser.openCsvBuilder(STATECODES_CSVFILE, StateCensus.class);
+        } catch (CensusAnalyserException e) {
+            e.printStackTrace();
+            Assert.assertEquals(CensusAnalyserException.CensusExceptionType.INCORRECT_DATA_ISSUE, e.type);
+        }
+    }
 }
