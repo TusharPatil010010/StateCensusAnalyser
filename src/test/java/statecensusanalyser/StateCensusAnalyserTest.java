2944d13661bbac04d5aceaab87c6c132e9c1096d
TusharPatil010010 <tusharp010010@gmail.com>
2020-10-25 16:47:10 +0530

    TC2.4 : Given CSV when correct but delimiter incorrect Returns a custom exception

--- src/test/java/statecensusanalyser/StateCensusAnalyserTest.java
+++ src/test/java/statecensusanalyser/StateCensusAnalyserTest.java
@@ -35,14 +35,27 @@ public class StateCensusAnalyserTest {
 			Assert.assertEquals(CensusAnalyserException.CensusExceptionType.NO_SUCH_FILE, e.type);
 		}
 	}
-	
-    @Test
-    public void GivenTheStateCodeCsvFile_WhenCorrect_ButFileExtensionIncorrect_ShouldThrowCensusAnalyserException() throws IOException {
-        try {
-            int count = StateCensusAnalyser.openCsvBuilderCode(STATECODES_CSVFILE, StateCensus.class);
-        } catch (CensusAnalyserException e) {
-            e.printStackTrace();
-            Assert.assertEquals(CensusAnalyserException.CensusExceptionType.INCORRECT_DATA_ISSUE, e.type);
-        }
-    }
+
+	@Test
+	public void GivenTheStateCodeCsvFile_WhenCorrect_ButFileExtensionIncorrect_ShouldThrowCensusAnalyserException()
+			throws IOException {
+		try {
+			int count = StateCensusAnalyser.openCsvBuilderCode(STATECODES_CSVFILE, StateCensus.class);
+		} catch (CensusAnalyserException e) {
+			e.printStackTrace();
+			Assert.assertEquals(CensusAnalyserException.CensusExceptionType.INCORRECT_DATA_ISSUE, e.type);
+		}
+	}
+
+	@Test
+	public void GivenTheStateCodeCSVFile_WhenCorrect_ButDelimiterIncorrect_ReturnsCensusAnalyserException()
+			throws IOException {
+		try {
+			int count = StateCensusAnalyser.openCsvBuilderCode(STATECODES_CSVFILE, StateCensus.class);
+		} catch (CensusAnalyserException e) {
+			e.printStackTrace();
+			Assert.assertEquals(CensusAnalyserException.CensusExceptionType.DELIMITER_ISSUE, e.type);
+
+		}
+	}
 }
