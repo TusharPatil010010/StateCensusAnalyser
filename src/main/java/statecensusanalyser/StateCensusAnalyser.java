2a51d9d437b32597a171b62fefd423d4f8a811df
TusharPatil010010 <tusharp010010@gmail.com>
2020-10-25 15:57:36 +0530

    Added functionality to load Indian States Census Information

--- src/main/java/statecensusanalyser/StateCensusAnalyser.java
+++ src/main/java/statecensusanalyser/StateCensusAnalyser.java
@@ -11,5 +11,45 @@ import java.nio.file.Paths;
 import java.util.Iterator;
 
 public class StateCensusAnalyser {
+    public static <T>  int openCsvBuilder(String csvIndiaStateCensus, Object myClass) throws CensusAnalyserException {
+        int counter = 0;
+        try {
+            Iterator<Object> myIterator = getIterator(csvIndiaStateCensus, myClass);
+            while ( myIterator.hasNext() ) {
+                counter++;
+                Object myObj = myIterator.next();
+                System.out.println(myObj.toString());
+            }
+        } catch (CensusAnalyserException e){
+            throw e;
+        } catch (RuntimeException e){
+            throw new CensusAnalyserException(CensusAnalyserException.CensusExceptionType.DELIMITER_ISSUE,
+                    "might be some error related to delimiter at record no. : " +(counter+1));
+        }
+        return counter;
+    }
 
+    public static Iterator<Object> getIterator(String csvIndiaStateCensus, Object myClass) throws CensusAnalyserException {
+        Reader reader = null;
+        CsvToBean<Object> csvToBean;
+        try {
+            reader = Files.newBufferedReader(Paths.get(csvIndiaStateCensus));
+            csvToBean = new CsvToBeanBuilder(reader)
+                    .withType((Class) myClass)
+                    .withIgnoreLeadingWhiteSpace(true)
+                    .build();
+            Iterator<Object> myIterator = csvToBean.iterator();
+            return myIterator;
+        } catch (NoSuchFileException e) {
+            throw new CensusAnalyserException(CensusAnalyserException.CensusExceptionType.NO_SUCH_FILE,
+                    "no such file exists. Please enter correct file");
+        } catch (RuntimeException e){
+            throw new CensusAnalyserException(CensusAnalyserException.CensusExceptionType.INCORRECT_DATA_ISSUE,
+                    "delimiter error at line 1 OR might be some error " +
+                            "related to headers or incorrect extension / input file ");
+        } catch (IOException e) {
+            throw new CensusAnalyserException(CensusAnalyserException.CensusExceptionType.SOME_OTHER_IO_EXCEPTION,
+                    "Some other IO related exception");
+        }
+    }
 }
